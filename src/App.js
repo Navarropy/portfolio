d8cace1cf5e21ac57b3e0301d2f34b3bfd6290eb
Adriano Navarro <Adriano_Navarro@outlook.com>
2022-03-25 18:45:17 -0300

    FIXED: Wrong news clone thumbnail.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Sidebar />
 
         <Projects>
-          <Project background="https://i.imgur.com/AMWoJuB.png" positiob="center" link="https://eager-darwin-1ca60b.netlify.app/"/>
+          <Project background="https://i.imgur.com/dk4aYlz.png" positiob="center" link="https://eager-darwin-1ca60b.netlify.app/"/>
           <Project background="https://i.imgur.com/WsstlRQ.png" position="center" link="https://codepen.io/navarropy/pen/RwjGdbb"/>
           <Project background="https://i.imgur.com/nyNeano.png" position="center" link="https://codepen.io/navarropy/pen/dyZpyyx"/>
           <Project background="https://i.imgur.com/Lypuuco.png" position="center" link="https://codepen.io/navarropy/pen/RwjoeGJ"/>
@@ -18,7 +18,7 @@ function App() {
           <Project background="https://i.imgur.com/vEVSdtY.png" position="center" link="https://codepen.io/navarropy/pen/gOXgGRd"/>
           <Project background="https://i.imgur.com/fUW1voO.png" position="center" link="https://codepen.io/navarropy/pen/vYWyymz?editors=0010"/>
           <Project background="https://i.imgur.com/lg65yEd.png" position="center" link="https://zealous-lichterman-8b1db8.netlify.app/"/>
-          <Project background="https://i.imgur.com/XxH5XRt.png" position="center" link="https://condescending-dubinsky-9e3c42.netlify.app/"/>
+          <Project background="https://i.imgur.com/QSiUddb.png" position="contain" link="https://condescending-dubinsky-9e3c42.netlify.app/"/>
         </Projects>
       </div>
     </>
