cecf5b3f5b28e9fc22790db7894d5247463d5b22
Navarropy <98678529+Navarropy@users.noreply.github.com>
2022-03-26 00:19:13 -0300

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -3,7 +3,7 @@ import "./style/project.scss"
 export default function Project(props) {
     return(
         <>
-            <a href={props.link} target="_blank">
+            <a href={props.link} target="_blank" rel="noreferrer">
                 <div className="project" style={{background: `url(${props.background})`, backgroundPosition: props.position, backgroundSize: "cover"}}></div>
             </a>
         </>
